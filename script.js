f6d2e37c3f450dbaf117f5c50ccd0ba8915772de
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-09-24 12:41:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+
+
+//Form to extract data
+let form = document.getElementsByTagName("form")[0];
+
+// Checking pass validity on btn click
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let p1 = document.getElementById("password");
+  let p2 = document.getElementById("confirm-password");
+
+  if (p1.value !== p2.value) {
+    alert("Passwords do not match");
+  } else {
+    let elements = form.children;
+    Array.from(elements).forEach((val) => {
+      if (
+        val.hasAttribute("id") &&
+        val.id != "confirm-password" &&
+        val.id != "submit"
+      ) {
+        sessionStorage.setItem(val.id, val.value);
+		
+      }
+	  val.value = "";
+    });
+
+    alert("Sign up successfull");
+  }
+});
+
+
